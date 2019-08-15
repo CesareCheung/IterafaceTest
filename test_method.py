0b3e17ef4ff94c0e07ada72ed75347627023dac6
zhangweixu <1689719986@qq.com>
2019-08-15 22:33:52 +0800

    unittest运用

--- test_method.py
+++ test_method.py
@@ -1,17 +1,48 @@
+import json
 import unittest
+
+from demo import RunMain
 
 
 class TestMethod(unittest.TestCase):
-    # 每次方法之前执行
-    def setUp(self):
-        print('test--->>>setUp')
 
-    # 每次方法后执行
+    def setUp(self):
+        self.run = RunMain()
+
+
     def tearDown(self):
         print('test--->>>tearDown')
 
     def test_01(self):
-        print('这个是测试方法！')
+        url = 'http://coding.imooc.com/api/cate'
+        data = {
+            'timestamp': '1507034803124',
+            'uid': '5249191',
+            'uuid': '5ae7d1a22c82fb89c78f603420870ad7',
+            'secrect': '078474b41dd37ddd5efeb04aa591ec12',
+            'token': '7d6f14f21ec96d755de41e6c076758dd',
+            'cid': '0',
+            'errorCode': 1001
+        }
+        res=self.run.run_method(url,"POST",data)
+        res=json.loads(res)
+        print(res)
+        self.assertEqual(res['errorCode'],1007,'测试失败')
+    def test_02(self):
+        url = 'http://coding.imooc.com/api/cate'
+        data = {
+            'timestamp': '1507034803124',
+            'uid': '5249191',
+            'uuid': '5ae7d1a22c82fb89c78f603420870ad7',
+            'secrect': '078474b41dd37ddd5efeb04aa591ec12',
+            'token': '7d6f14f21ec96d755de41e6c076758dd',
+            'cid': '0'
+        }
+
+        res = json.loads(self.run.run_method(url, 'POST', data))
+        self.assertEqual(res['errorCode'], 1007, "测试失败")
+        print(res)
+
 
 if __name__ == '__main__':
     unittest.main()
