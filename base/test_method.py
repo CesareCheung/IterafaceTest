e728136344f8aba854913e61a900584bd92e8afc
zhangweixu <1689719986@qq.com>
2019-08-17 20:04:15 +0800

    unittest及mock模拟运用

--- base/test_method.py
+++ base/test_method.py
@@ -1,7 +1,8 @@
 import json
 import unittest
-# from base.HTMLTestRunner import HTMLTestRunner
-from base.HTMLTestRunnerCN import HTMLTestRunner
+from unittest import mock
+from base.mock_demo import mock_test
+from HTMLTestRunnerCN import HTMLTestRunner
 import HTMLReport
 
 from base.demo import RunMain
@@ -28,11 +29,13 @@ class TestMethod(unittest.TestCase):
             'cid': '0',
             'errorCode': 1001
         }
-        res = self.run.run_method(url, "POST", data)
-        res = json.loads(res)
+        # self.run.run_method = mock.Mock(return_value=data)
+        # res = self.run.run_method(url, "POST", data)
+        res = mock_test(self.run.run_method,data, url, "POST", data)
+        # res = json.loads(res)
         print(res)
         print("第一个case")
-        self.assertEqual(res['errorCode'], 1007, '测试失败')
+        self.assertEqual(res['errorCode'], 1001, '测试失败')
 
     def test_02(self):
         url = 'http://coding.imooc.com/api/cate'
@@ -42,11 +45,12 @@ class TestMethod(unittest.TestCase):
             'uuid': '5ae7d1a22c82fb89c78f603420870ad7',
             'secrect': '078474b41dd37ddd5efeb04aa591ec12',
             'token': '7d6f14f21ec96d755de41e6c076758dd',
-            'cid': '0'
+            'cid': '0',
+            'errorCode':1000
         }
-
-        res = json.loads(self.run.run_method(url, 'POST', data))
-        self.assertEqual(res['errorCode'], 1007, "测试失败")
+        res=mock_test(self.run.run_method,data,url,"POST",data)
+        # res = json.loads(self.run.run_method(url, 'POST', data))
+        self.assertEqual(res['errorCode'], 1000, "测试失败")
         print(res)
         print("第二个case")
 
@@ -59,7 +63,7 @@ if __name__ == '__main__':
     with open(fithpath, 'wb') as fp:
         runner = HTMLTestRunner(
             stream=fp,
-            title='测试报告的标题:',
+            title='测试报告的标题:接口自动化测试',
             description='测试报告的描述:这是两个测试用例')
         runner.run(suite)
 
