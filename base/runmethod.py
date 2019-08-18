14f4708a15c14fd6aec58e0027bcdade41761ea6
zhangweixu <1689719986@qq.com>
2019-08-18 23:20:51 +0800

    更新post和get基类封装

--- base/runmethod.py
+++ base/runmethod.py
@@ -1,16 +1,9 @@
 import requests
+import json
 
 
 class RunMethod:
-
     def post_main(self, url, data, header=None):
-        """
-        post请求方式
-        :param url:请求地址
-        :param data:请求数据
-        :param header:请求头部
-        :return:
-        """
         res = None
         if header != None:
             res = requests.post(url=url, data=data, headers=header)
@@ -19,13 +12,6 @@ class RunMethod:
         return res.json()
 
     def get_main(self, url, data=None, header=None):
-        """
-        get请求方式
-        :param url:请求地址
-        :param data:请求数据
-        :param header:请求头部
-        :return:
-        """
         res = None
         if header != None:
             res = requests.get(url=url, data=data, headers=header, verify=False)
@@ -34,17 +20,19 @@ class RunMethod:
         return res.json()
 
     def run_main(self, method, url, data=None, header=None):
-        """
-        运行函数
-        :param method:请求方式
-        :param url:请求地址
-        :param data:请求数据
-        :param header:请求头部
-        :return:
-        """
         res = None
-        if method == "post":
+        if method == 'Post':
             res = self.post_main(url, data, header)
         else:
             res = self.get_main(url, data, header)
-        return res
+        return json.dumps(res, indent=2, sort_keys=True, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    url = 'http://httpbin.org/get'
+    data = {
+        'cart': '11'
+    }
+    run = RunMethod()
+    run_test = run.run_main(method="GET", url=url, data=data)
+    print(run_test)
