5f2cc711222dd18e58482259460dc9fd5a7c63d9
zhangweixu <1689719986@qq.com>
2019-08-24 19:39:39 +0800

    发送邮件报告

--- util/send_email.py
+++ util/send_email.py
@@ -8,8 +8,8 @@ class SendEmail:
     global email_host
     global password
     send_user = "xxxx@163.com"
-    email_host="smtp.163.com"
-    password='xxxxxx'
+    email_host = "smtp.163.com"
+    password = 'xxxxxx'
 
     def send_mail(self, user_list, sub, content):
         """
@@ -21,18 +21,19 @@ class SendEmail:
         """
         user = "xxxx.com" + "<" + send_user + ">"
         message = MIMEText(content, _subtype="plain", _charset="utf-8")
-        message['Subject']=sub
-        message['From']=user
-        message['To']=";".join(user_list)
-        server=smtplib.SMTP()
+        message['Subject'] = sub
+        message['From'] = user
+        message['To'] = ";".join(user_list)
+        server = smtplib.SMTP()
         server.connect(email_host)
-        server.login(send_user,password )
-        server.sendmail(user,user_list,message.as_string())
+        server.login(send_user, password)
+        server.sendmail(user, user_list, message.as_string())
         server.close()
 
+
 if __name__ == '__main__':
-    send=SendEmail()
-    user_list=['1689719986@qq.com']
-    sub='测试主题'
-    content="测试"
-    send.send_mail(user_list,sub,content)
+    send = SendEmail()
+    user_list = ['1689719986@qq.com']
+    sub = '测试主题'
+    content = "测试"
+    send.send_mail(user_list, sub, content)
