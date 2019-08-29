cff194a07a25c3a67274d6a3ac6859cc0dd10419
zhangweixu <1689719986@qq.com>
2019-08-29 18:18:28 +0800

    发送邮件及添加附件方法封装

--- util/send_email.py
+++ util/send_email.py
@@ -1,4 +1,5 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
@@ -19,11 +20,19 @@ class SendEmail:
         :param content: 内容
         :return:
         """
-        user = "@163.com" + "<" + send_user + ">"
-        message = MIMEText(content, _subtype="plain", _charset="utf-8")
+        user = "XXX@163.com" + "<" + send_user + ">"
+        message = MIMEMultipart()
         message['Subject'] = sub
         message['From'] = user
-        message['To'] = ";".join(user_list)
+        message['To'] = ",".join(user_list)
+        body = MIMEText(content, _subtype="plain", _charset="utf-8")  # 邮件内容
+        message.attach(body)
+        # 添加附件
+        att = MIMEText(open("../dataconfig/case1.xls", "rb").read(), "base64",
+                       "utf-8")  # 打开附件地址
+        att["Content-Type"] = "application/octet-stream"
+        att["Content-Disposition"] = 'attachment; filename="case1.xls"'
+        message.attach(att)
         server = smtplib.SMTP()
         server.connect(email_host)
         server.login(send_user, password)
@@ -35,20 +44,20 @@ class SendEmail:
         发送报告
         :param pass_list:
         :param fail_list:
-        :return:XXXXXX
+        :return:
         """
         pass_num = float(len(pass_list))
         fail_num = float(len(fail_list))
         count_num = pass_num + fail_num
         pass_result = "%.2f%%" % (pass_num / count_num * 100)
         fail_result = "%.2f%%" % (fail_num / count_num * 100)
-        user_list = ['@qq.com']
+        user_list = ['hhhh@qq.com','aaaa@qq.com']
 
         sub = '接口自动化测试报告'
-        content = f"此次接口测试一共运行用例为：{count_num}，通过个数为：{pass_num}，失败个数为：{fail_num}，通过率为：{pass_result}"
+        content = f"此次接口测试一共运行用例为：{count_num}，通过个数为：{pass_num}，失败个数为：{fail_num}，通过率为：{pass_result},报告详情请查看附件！"
         self.send_mail(user_list, sub, content)
 
 
 if __name__ == '__main__':
     send = SendEmail()
-    send.send_main([1,2,3,4],[1,2])
+    send.send_main([1, 2, 3, 4], [6, 2])
