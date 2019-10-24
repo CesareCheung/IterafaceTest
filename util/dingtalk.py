ced0054e79051472327a6c89fdee5576d21acbd2
zhangweixu <1689719986@qq.com>
2019-10-24 11:41:34 +0800

    钉钉

--- util/dingtalk.py
+++ util/dingtalk.py
@@ -10,8 +10,8 @@ class SendText:
         # Text消息@所有人
         self.xiaoding.send_text(msg='我是打包机器人！', is_at_all=False)
 
-        self.xiaoding.send_link(title='苹果打包地址', text='测试环境', message_url='')
-        self.xiaoding.send_link(title='安卓打包地址', text='测试环境', message_url='')
+        self.xiaoding.send_link(title='苹果打包地址', text='测试环境', message_url='https://www.com')
+        self.xiaoding.send_link(title='安卓打包地址', text='测试环境', message_url='https://www..com')
 
 
 if __name__ == '__main__':
